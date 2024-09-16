a773b272f87e85a9da86ed00aff748297f3426d9
lucasemmanuelsa <lucasemmanuelsa@gmail.com>
2024-09-16 19:06:41 -0300

    feat: response to the client from server when publish

---
+++ client/client.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+    "fmt"
+    "io"
+    "net"
+    "os"
+    "strings"
+    "time"
+)
+
+func main() {
+    command := os.Args[1]
+    serverIp := "localhost:5000" // Modificar quando o real ip do servidor for definido
+    if command == "search" {
+        fileHash := os.Args[2]
+        search(fileHash, serverIp)
+    } else if command == "publish" {
+        operationAndFileHashs := strings.Join(os.Args[2:], " ")
+        publish(operationAndFileHashs, serverIp)
+    }
+}
+
+func search(fileHash string, serverIp string) {
+    conn, err := net.Dial("tcp", serverIp)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    defer conn.Close()
+
+    _, err = conn.Write([]byte("search " + fileHash))
+    if err != nil {
+        fmt.Println("Erro ao enviar dados para o servidor:", err)
+        return
+    }
+}
+
+func publish(operationAndFileHashs string, serverIp string) {
+    clientIp, err := getClientIP()
+    if err != nil {
+        fmt.Println("Erro ao obter o IP do cliente:", err)
+        return
+    }
+
+    conn, err := net.Dial("tcp", serverIp)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    defer conn.Close()
+
+    fmt.Println("IP do cliente:", clientIp)
+
+    // Pausa de 2 segundos antes de enviar a solicitação
+    time.Sleep(2 * time.Second)
+
+    _, err = conn.Write([]byte(clientIp + " publish " + operationAndFileHashs))
+    if err != nil {
+        fmt.Println("Erro ao enviar dados para o servidor:", err)
+        return
+    }
+
+    fmt.Println("Dados enviados para o servidor")
+    // Lê a resposta do servidor
+    fmt.Println("Aguardando resposta do servidor...")
+
+    response, err := io.ReadAll(conn)
+    if err != nil {
+        fmt.Println("Erro ao ler resposta do servidor:", err)
+        return
+    }
+
+    fmt.Println("Resposta do servidor:", string(response))
+}
+
+func getClientIP() (string, error) {
+    addrs, err := net.InterfaceAddrs()
+    if err != nil {
+        return "", err
+    }
+
+    for _, addr := range addrs {
+        // Verifica se o endereço é do tipo IP e não é um loopback
+        if ipNet, ok := addr.(*net.IPNet); ok && !ipNet.IP.IsLoopback() {
+            if ipNet.IP.To4() != nil {
+                return ipNet.IP.String(), nil
+            }
+        }
+    }
+
+    return "", fmt.Errorf("não foi possível obter o IP do cliente")
+}
--- server/server.go
+++ server/server.go
@@ -1,76 +1,112 @@
 package main
 
 import (
-	"log"
-	"net"
-    "io"
-    "strings"
     "fmt"
+    "io"
+    "log"
+    "net"
+    "strings"
 )
 
 func main() {
-    fileHashs := make(map [string][]string)
+    listener, err := net.Listen("tcp", ":5000")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer listener.Close()
+
     channelDiffs := make(chan []string)
-	listener, err := net.Listen("tcp", "localhost:8000")
-	if err != nil {
-		log.Fatal(err)
-	}
+    fileHashs := make(map[string][]string)
+
     go listenDiffs(channelDiffs, fileHashs)
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Print(err)
-			continue
-		}
-        request, errRead := io.ReadAll(conn)
-        if errRead != nil {
-            log.Print(errRead)
+
+    for {
+        conn, err := listener.Accept()
+        if err != nil {
+            log.Print(err)
+            continue
         }
-        data := strings.Split(string(request), " ")
-        switch command := data[1]; command {
-            case "publish":
-                log.Print("entrou no publish")
-                go handlePublish(data, channelDiffs)
-            case "find":
-                // TODO: implementar aqui
-                log.Print("entrou no find")
-                // go handleFind(conn, data, fileHashs)
-            default:
-                log.Print("descartando comando inválido")
+        go handleConnection(conn, channelDiffs)
+    }
+}
+
+func handleConnection(conn net.Conn, channelDiffs chan []string) {
+    defer conn.Close()
+    buffer := make([]byte, 1024)
+    n, err := conn.Read(buffer)
+    if err != nil {
+        if err != io.EOF {
+            log.Print(err)
         }
-	}
+        return
+    }
+    data := strings.Split(string(buffer[:n]), " ")
+    if len(data) < 2 {
+        log.Print("descartando comando inválido")
+        return
+    }
+    switch command := data[1]; command {
+    case "publish":
+        log.Print("entrou no publish")
+        handlePublish(conn, data, channelDiffs)
+    case "find":
+        log.Print("entrou no find")
+        // Implementar handleFind aqui
+    default:
+        log.Print("descartando comando inválido")
+    }
+}
+
+func handlePublish(conn net.Conn, data []string, channelDiffs chan []string) {
+    for _, diff := range data[2:] {
+        channelDiffs <- []string{data[0], diff}
+    }
+
+    fmt.Println("teste")
+    // Envia uma mensagem de confirmação ao cliente
+    _, err := conn.Write([]byte("Itens adicionados com sucesso"))
+    if err != nil {
+        log.Print("Erro ao enviar confirmação ao cliente:", err)
+    }
+}
+
+func removeElem(list []string, elem string) []string {
+    newList := []string{}
+    for _, current := range list {
+        if current != elem {
+            newList = append(newList, current)
+        }
+    }
+    return newList
 }
 
 func listenDiffs(channelDiffs chan []string, fileHashs map[string][]string) {
     for {
-        diff, _ := <- channelDiffs
-        ipAddress := diff[0] 
+        diff := <-channelDiffs
+        ipAddress := diff[0]
         command := strings.Split(diff[1], ",")
         fileHash := command[1]
         switch command[0] {
-            case "a":
+        case "a":
+            if !contains(fileHashs[fileHash], ipAddress) {
                 fileHashs[fileHash] = append(fileHashs[fileHash], ipAddress)
                 log.Print("file added")
-            case "r":
-                fileHashs[fileHash] = removeElem(fileHashs[fileHash], ipAddress)
-                log.Print("file removed")
+            } else {
+                log.Print("IP já presente no array")
+            }
+        case "r":
+            fileHashs[fileHash] = removeElem(fileHashs[fileHash], ipAddress)
+            log.Print("file removed")
         }
         fmt.Println(fileHashs)
     }
 }
 
-func removeElem(list []string, elem string) [] string {
-    newList := []string {}
-    for _, current := range list {
-        if current != elem {
-            newList = append(newList, current)
+func contains(list []string, elem string) bool {
+    for _, item := range list {
+        if item == elem {
+            return true
         }
-    } 
-    return newList
-}
-
-func handlePublish(data []string, channelDiffs chan []string) {
-    for _, diff := range(data[2:]) {
-        channelDiffs <- []string { data[0], diff }
     }
+    return false
 }
