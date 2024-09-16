541c6a193a9d79ee8c4b103fe832b9f470975988
GabrielYuriRF0 <gabrie.yuri1020@gmail.com>
2024-09-15 21:19:58 -0300

    feat: adicioando método de publish no lado do cliente

--- client/client.go
+++ client/client.go
@@ -8,12 +8,17 @@ import (
 
 func main() {
 	command := os.Args[1]
-
+	serverIp := "localhost:8080" // Modificar quando o real ip do servidor for definido
 	if command == "search" {
 		fileHash := os.Args[2]
-		serverIp := "localhost:8080" // Modificar quando o real ip do servidor for definido
+
 		search(fileHash, serverIp)
+	} else if command == "publish" {
+		operationAndFileHashs := os.Args[2]
+		publish(operationAndFileHashs, serverIp)
+
 	}
+
 }
 
 func search(fileHash string, serverIp string) {
@@ -23,10 +28,28 @@ func search(fileHash string, serverIp string) {
 		return
 	}
 
-	_, err = conn.Write([]byte(fileHash + "\n"))
+	defer conn.Close()
+
+	_, err = conn.Write([]byte("search " + fileHash))
 
 	if err != nil {
+		fmt.Println("Erro ao enviar dados para o servidor:", err)
+		return
+	}
+}
+
+func publish(operationAndFileHashs string, serverIp string) {
+	conn, err := net.Dial("tcp", serverIp)
+	if err != nil {
 		fmt.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	_, err = conn.Write([]byte("publish " + operationAndFileHashs))
+
+	if err != nil {
+		fmt.Println("Erro ao enviar dados para o servidor:", err)
 		return
 	}
 
