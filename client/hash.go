171d37e45b950f61f1a6bbc3665fa48739a77f87
Enzo Vasconcelos <enzo.vasconcelos@ccc.ufcg.edu.br>
2024-09-17 13:32:11 -0300

    fix: fixing hash script file

--- client/hash.go
+++ client/hash.go
@@ -3,6 +3,7 @@ package main
 import (
     "fmt"
     "os"
+    "io"
 )
 
 type ResultCalculate struct {
@@ -36,17 +37,23 @@ func calculateHash(fileName string, directory string, join chan bool) {
     bytes, err := os.ReadFile(fmt.Sprintf("%s/%s", directory, fileName))
     if err != nil {
         fmt.Println(err)
+        os.Exit(1)
     }
     var sum int
     for _, byteReaded := range bytes {
+        fmt.Println(byteReaded)
         sum += int(byteReaded)
     }
 
     filePath := fmt.Sprintf("%s/%s.hash", directory, fileName)
     fmt.Println(fmt.Sprintf("writing %d hash", sum))
-    errWrite := os.WriteFile(filePath, []byte(string(sum)), 0777)
+    fmt.Println(string(sum))
+    var buf [] byte
+    binary.Encoder(buf, uint8, sum) 
+    errWrite := os.WriteFile(filePath, buf, 0777)
     if errWrite != nil {
         fmt.Println(errWrite)
+        os.Exit(1)
     }
     join <- true
 }
